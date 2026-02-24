a57f2c57a48d26c48eeeb012523e13765e5709d0
Jose Ramos <144376748+jjoseramoss@users.noreply.github.com>
2026-02-24 09:02:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tweeter Clone - Web Deb Project 1
+
+- Jose Ramos
+- 2/24/2026
---
+++ js/tweeter.js
@@ -0,0 +1,56 @@
+var input = document.getElementById("input-tweet");
+
+var displayChar = document.getElementById("remaining-char");
+var tweetBtn = document.getElementById("tweet-btn");
+var tweetLabel = document.getElementById("tweet-label");
+
+const totalChar = 160;
+var remainingChar = 160;
+
+// Enforce a 160-character limit per tweet  
+// If the limit is exceeded, prevent submission by disabling the ‘tweet’ button  
+// Use the text in the label to show a clear message about this 160-char limit
+function validateInput(i) {
+
+  // Get Remaining Characters left
+  var newInputLength = input.value.length
+  remainingChar = totalChar - newInputLength;
+  
+  // Disable tweet btn if no more char available
+  if (remainingChar < 0) {
+
+    displayChar.style.color = "red";
+
+    // Add disabled attribute
+    tweetBtn.setAttribute("disabled", "");
+
+    // Disabled styling
+    tweetBtn.classList.add("disabledBtn");
+    tweetBtn.classList.remove("btn-hover");
+
+    // Display label message 
+    tweetLabel.innerHTML = "Please Reduce Message"
+  } 
+  // User has characters remaining
+  else {
+    displayChar.style.color = "black";
+    tweetBtn.removeAttribute("disabled", "");
+    tweetBtn.classList.remove("disabledBtn");
+    tweetBtn.classList.add("btn-hover");
+    tweetLabel.innerHTML = ""
+  }
+}
+
+function updateText(){
+  inputSize = input.value.length
+  remainingChar = totalChar - inputSize
+
+  // Update number of remaining char
+  displayChar.textContent = remainingChar
+
+
+}
+
+function submit(){
+  alert(input.value);
+}
