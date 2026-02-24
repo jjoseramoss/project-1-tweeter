7f4ca0fa38da50651a5d77efb89b68c6eb23a9a8
Jose Ramos <144376748+jjoseramoss@users.noreply.github.com>
2026-02-24 10:38:53 -0600

    Updates to styling

--- js/tweeter.js
+++ js/tweeter.js
@@ -7,18 +7,16 @@ var tweetLabel = document.getElementById("tweet-label");
 const totalChar = 160;
 var remainingChar = 160;
 
-// Enforce a 160-character limit per tweet  
-// If the limit is exceeded, prevent submission by disabling the ‘tweet’ button  
+// Enforce a 160-character limit per tweet
+// If the limit is exceeded, prevent submission by disabling the ‘tweet’ button
 // Use the text in the label to show a clear message about this 160-char limit
 function validateInput(i) {
-
   // Get Remaining Characters left
-  var newInputLength = input.value.length
+  var newInputLength = input.value.length;
   remainingChar = totalChar - newInputLength;
-  
+
   // Disable tweet btn if no more char available
   if (remainingChar < 0) {
-
     displayChar.style.color = "red";
 
     // Add disabled attribute
@@ -28,29 +26,27 @@ function validateInput(i) {
     tweetBtn.classList.add("disabledBtn");
     tweetBtn.classList.remove("btn-hover");
 
-    // Display label message 
-    tweetLabel.innerHTML = "Please Reduce Message"
-  } 
+    // Display label message
+    tweetLabel.innerHTML = `Limit Reached: ${totalChar} characters max`;
+  }
   // User has characters remaining
   else {
     displayChar.style.color = "black";
     tweetBtn.removeAttribute("disabled", "");
     tweetBtn.classList.remove("disabledBtn");
     tweetBtn.classList.add("btn-hover");
-    tweetLabel.innerHTML = ""
+    tweetLabel.innerHTML = "";
   }
 }
 
-function updateText(){
-  inputSize = input.value.length
-  remainingChar = totalChar - inputSize
+function updateText() {
+  inputSize = input.value.length;
+  remainingChar = totalChar - inputSize;
 
   // Update number of remaining char
-  displayChar.textContent = remainingChar
-
-
+  displayChar.textContent = remainingChar;
 }
 
-function submit(){
+function submit() {
   alert(input.value);
 }
